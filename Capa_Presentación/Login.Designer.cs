b32ce401a34d3353957d5ab1b296877944b43b79
IvyMasterDiego <diegolobato82@gmail.com>
2022-01-13 13:46:31 -0400

    Modificacion para que se pueda ingresar datos en la tabla

--- Capa_Presentación/Login.Designer.cs
+++ Capa_Presentación/Login.Designer.cs
@@ -44,10 +44,9 @@ namespace Capa_Presentación
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(51, 162);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(38, 132);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(288, 23);
+            this.label1.Size = new System.Drawing.Size(229, 19);
             this.label1.TabIndex = 1;
             this.label1.Text = "Payroll Management System";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -55,31 +54,29 @@ namespace Capa_Presentación
             // txtUser
             // 
             this.txtUser.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUser.Location = new System.Drawing.Point(48, 235);
-            this.txtUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUser.Location = new System.Drawing.Point(36, 191);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(319, 27);
+            this.txtUser.Size = new System.Drawing.Size(240, 23);
             this.txtUser.TabIndex = 2;
             // 
             // txtPass
             // 
             this.txtPass.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPass.Location = new System.Drawing.Point(48, 311);
-            this.txtPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPass.Location = new System.Drawing.Point(36, 253);
             this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(319, 27);
+            this.txtPass.Size = new System.Drawing.Size(240, 23);
             this.txtPass.TabIndex = 3;
             this.txtPass.UseSystemPasswordChar = true;
+            this.txtPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPass_KeyPress_1);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(44, 212);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(33, 172);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.Size = new System.Drawing.Size(41, 16);
             this.label2.TabIndex = 4;
             this.label2.Text = "User";
             // 
@@ -88,10 +85,9 @@ namespace Capa_Presentación
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label3.Location = new System.Drawing.Point(44, 288);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(33, 234);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.Size = new System.Drawing.Size(76, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "Password";
             // 
@@ -103,10 +99,9 @@ namespace Capa_Presentación
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(131, 378);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(98, 307);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 41);
+            this.button1.Size = new System.Drawing.Size(119, 33);
             this.button1.TabIndex = 6;
             this.button1.Text = "Iniciar Sesión ";
             this.button1.UseVisualStyleBackColor = false;
@@ -118,10 +113,9 @@ namespace Capa_Presentación
             this.lblErrorMessage.Font = new System.Drawing.Font("MS Reference Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblErrorMessage.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.lblErrorMessage.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.lblErrorMessage.Location = new System.Drawing.Point(44, 356);
-            this.lblErrorMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblErrorMessage.Location = new System.Drawing.Point(33, 289);
             this.lblErrorMessage.Name = "lblErrorMessage";
-            this.lblErrorMessage.Size = new System.Drawing.Size(109, 18);
+            this.lblErrorMessage.Size = new System.Drawing.Size(89, 15);
             this.lblErrorMessage.TabIndex = 7;
             this.lblErrorMessage.Text = "Error Message";
             this.lblErrorMessage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -131,20 +125,19 @@ namespace Capa_Presentación
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Image = global::Capa_Presentación.Properties.Resources.salary1;
-            this.pictureBox1.Location = new System.Drawing.Point(131, 15);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(98, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(159, 127);
+            this.pictureBox1.Size = new System.Drawing.Size(119, 103);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FloralWhite;
-            this.ClientSize = new System.Drawing.Size(409, 490);
+            this.ClientSize = new System.Drawing.Size(307, 398);
             this.Controls.Add(this.lblErrorMessage);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
@@ -155,7 +148,6 @@ namespace Capa_Presentación
             this.Controls.Add(this.pictureBox1);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
